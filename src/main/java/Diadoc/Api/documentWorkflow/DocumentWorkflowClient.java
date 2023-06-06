0bff72caf8a77cba912c6c22e56ab2ec548cbc7c
anatoli.dubko <anatoli.dubko@skbkontur.ru>
2023-06-06 19:53:55 +0500

    up getworkflowsettings version

--- src/main/java/Diadoc/Api/documentWorkflow/DocumentWorkflowClient.java
+++ src/main/java/Diadoc/Api/documentWorkflow/DocumentWorkflowClient.java
@@ -6,7 +6,7 @@ import java.net.URISyntaxException;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.client.utils.URIBuilder;
 
-import Diadoc.Api.Proto.Workflows.DocumentWorkflowSettingsProtos.DocumentWorkflowSettingsList;
+import Diadoc.Api.Proto.Workflows.DocumentWorkflowSettingsProtos.DocumentWorkflowSettingsListV2;
 import Diadoc.Api.exceptions.DiadocSdkException;
 import Diadoc.Api.httpClient.DiadocHttpClient;
 
@@ -17,19 +17,19 @@ public class DocumentWorkflowClient {
         this.diadocHttpClient = diadocHttpClient;
     }
 
-    public DocumentWorkflowSettingsList getWorkflowsSettings(String boxId) throws DiadocSdkException {
+    public DocumentWorkflowSettingsListV2 getWorkflowsSettings(String boxId) throws DiadocSdkException {
         if (boxId == null) {
             throw new IllegalArgumentException("boxId");
         }
 
         try {
             var url = new URIBuilder(diadocHttpClient.getBaseUrl())
-                    .setPath("/GetWorkflowsSettings")
+                    .setPath("/V2/GetWorkflowsSettings")
                     .addParameter("boxId", boxId);
 
             var request = RequestBuilder.get(url.build());
 
-            return DocumentWorkflowSettingsList.parseFrom(diadocHttpClient.performRequest(request));
+            return DocumentWorkflowSettingsListV2.parseFrom(diadocHttpClient.performRequest(request));
         } catch (URISyntaxException | IOException e) {
             throw new DiadocSdkException(e);
         }
