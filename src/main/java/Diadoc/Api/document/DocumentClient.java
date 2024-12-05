6788b593b9a5c8ec7cf5f1c09a94cde01fe8068c
София Корватовская <korvatovskaya.s@skbkontur.ru>
2024-12-05 15:35:19 +0500

    client added and tools refactored

--- src/main/java/Diadoc/Api/document/DocumentClient.java
+++ src/main/java/Diadoc/Api/document/DocumentClient.java
@@ -7,6 +7,7 @@ import Diadoc.Api.httpClient.DiadocHttpClient;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ByteArrayEntity;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -277,6 +278,33 @@ public class DocumentClient {
                             .addParameter("orgId", orgId)
                             .build());
             return ResolutionRouteListProtos.ResolutionRouteList.parseFrom(diadocHttpClient.performRequest(request));
+        } catch (URISyntaxException | IOException e) {
+            throw new DiadocSdkException(e);
+        }
+    }
+
+    public void restore(String boxId, String messageId, @Nullable String documentId) throws DiadocSdkException {
+        if (Tools.isNullOrEmpty(boxId)) {
+            throw new IllegalArgumentException("boxId");
+        }
+
+        if (Tools.isNullOrEmpty(messageId)) {
+            throw new IllegalArgumentException("messageId");
+        }
+
+        try {
+            var uri = new URIBuilder(diadocHttpClient.getBaseUrl())
+                    .setPath("/Restore")
+                    .addParameter("boxId", boxId)
+                    .addParameter("messageId", messageId);
+
+            if (documentId != null) {
+                uri.addParameter("documentId", documentId);
+            }
+
+            var request = RequestBuilder.post(uri.build());
+
+            diadocHttpClient.performRequest(request);
         } catch (URISyntaxException | IOException e) {
             throw new DiadocSdkException(e);
         }
